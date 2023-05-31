8e24461572d9a1876775b358694151f2cb02d002
DuyTinhCam <k205480106011@tnut.edu.vn>
2023-05-31 08:25:17 +0700

    Add project files.

---
+++ TAOBOT8/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace TAOBOT8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(177, 80);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(439, 223);
+            this.txtLog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(954, 433);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TAOBOT8/ketnoivoianh.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAOBOT8
+{
+    public class ketnoivoianh
+    {
+        static string strCon = @"Data Source=DESKTOP-38UPOVL\SQLEXPRESS;Initial Catalog=SQL_QLBH;Integrated Security=True";
+        public static string Tim_mh(string tenMH)
+        {
+            string kq = "";
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(strCon))
+                {
+                    cn.Open();
+                    using (SqlCommand cm = cn.CreateCommand())
+                    {
+                        cm.CommandText = "Tim_mh";
+                        cm.CommandType = CommandType.StoredProcedure;
+                        cm.Parameters.Add("@tenMH", SqlDbType.NVarChar, 50).Value = tenMH;
+                        object obj = cm.ExecuteScalar(); //lấy col1 of row1
+                        if (obj != null)
+                            kq = (string)obj;
+                        else
+                            kq = $"không có MH nào tên: {tenMH}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                kq += $"Error: {ex.Message}";
+            }
+            return kq;
+        }
+    }
+
+}
